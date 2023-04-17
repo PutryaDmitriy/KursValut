36c9575e38a07a23feea68cd98334a497cb8f09e
PutryaDmitriy <122860630+PutryaDmitriy@users.noreply.github.com>
2023-04-17 20:42:09 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+#kursvalutPD
+TOKEN = '6054804196:AAHK7Vpc0AlCRoWAbMKK9GTcD4H6FE9o3Ds'
+
+# Для extesions.py
+keys = {
+    'BTC': 'BTC',
+    'ETH': 'ETH',
+    'USD': 'USD',
+    'EUR': 'EUR',
+    'RUB': 'RUB',
+}
+
+# Для main.py
+keys2 = {
+    '₿ Биткоин': 'BTC',
+    'Ξ Эфириум': 'ETH',
+    '$ Доллар': 'USD',
+    '€ Евро': 'EUR',
+    '₽ Рубль': 'RUB',
+}
